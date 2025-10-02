dd314bbcc1ea0d52d20c416b9b0cf51ddc1552f2
Haruka-LR <zomberoswaldo67@gmail.com>
2025-10-02 13:39:33 -0600

    Se agrega registro y inicio

--- polls/views/views.py
+++ polls/views/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def login_view(request):
     return render(request, 'polls/login.html')
+
+def inicio_view(request):
+    return render(request, 'polls/inicio.html')
+def register_view(request):
+    return render(request, 'polls/registro.html')
