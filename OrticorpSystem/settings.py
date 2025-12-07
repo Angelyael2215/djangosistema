d0a37022407af7d1e08dc21042881b1d21f137b2
yael382 <irvinyael382@gmail.com>
2025-12-07 14:56:39 -0600

    Restore INSTALLED_APPS/polls and AUTH_USER_MODEL + MEDIA settings after restoring DB config

--- OrticorpSystem/settings.py
+++ OrticorpSystem/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # App interno
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Use the custom user model from polls
+AUTH_USER_MODEL = 'polls.CustomUser'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
